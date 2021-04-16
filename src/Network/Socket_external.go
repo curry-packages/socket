058ffe00ff16ac0ae2998c0d0decefe54f3fd298
Michael Hanus <mh@informatik.uni-kiel.de>
2021-04-16 16:34:41 +0200

    Stub implementation for curry2go inserted

---
+++ src/Network/Socket_external.go
@@ -0,0 +1,28 @@
+package NetworkSocket
+
+import "gocurry"
+
+
+func ExternalNetwork_Socket_prim_listenOn(task *gocurry.Task){
+    panic("Network.Socket.listenOn: not yet implemented")
+}
+
+func ExternalNetwork_Socket_listenOnFresh(task *gocurry.Task){
+    panic("Network.Socket.listenOnFresh: not yet implemented")
+}
+
+func ExternalNetwork_Socket_prim_socketAccept(task *gocurry.Task){
+    panic("Network.Socket.socketAccept: not yet implemented")
+}
+
+func ExternalNetwork_Socket_prim_waitForSocketAccept(task *gocurry.Task){
+    panic("Network.Socket.witForSocketAccept: not yet implemented")
+}
+
+func ExternalNetwork_Socket_prim_sClose(task *gocurry.Task){
+    panic("Network.Socket.sClose: not yet implemented")
+}
+
+func ExternalNetwork_Socket_prim_connectToSocket(task *gocurry.Task){
+    panic("Network.Socket.connectToSocket: not yet implemented")
+}
