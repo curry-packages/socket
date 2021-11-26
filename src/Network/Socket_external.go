a4d927547b25256e228301eae64d898d005fb8f8
Michael Hanus <mh@informatik.uni-kiel.de>
2021-11-26 22:47:49 +0100

    Adapt to new name scheme

--- src/Network/Socket_external.go
+++ src/Network/Socket_external.go
@@ -1,28 +1,28 @@
-package NetworkSocket
+package Network_DOT_Socket
 
 import "gocurry"
 
 
-func ExternalNetwork_Socket_prim_listenOn(task *gocurry.Task){
+func ExternalNetworkDot_SocketDot_primUs_listenOn(task *gocurry.Task){
     panic("Network.Socket.listenOn: not yet implemented")
 }
 
-func ExternalNetwork_Socket_listenOnFresh(task *gocurry.Task){
+func ExternalNetworkDot_SocketDot_listenOnFresh(task *gocurry.Task){
     panic("Network.Socket.listenOnFresh: not yet implemented")
 }
 
-func ExternalNetwork_Socket_prim_socketAccept(task *gocurry.Task){
+func ExternalNetworkDot_SocketDot_primUs_socketAccept(task *gocurry.Task){
     panic("Network.Socket.socketAccept: not yet implemented")
 }
 
-func ExternalNetwork_Socket_prim_waitForSocketAccept(task *gocurry.Task){
-    panic("Network.Socket.witForSocketAccept: not yet implemented")
+func ExternalNetworkDot_SocketDot_primUs_waitForSocketAccept(task *gocurry.Task){
+    panic("Network.Socket.waitForSocketAccept: not yet implemented")
 }
 
-func ExternalNetwork_Socket_prim_sClose(task *gocurry.Task){
+func ExternalNetworkDot_SocketDot_primUs_sClose(task *gocurry.Task){
     panic("Network.Socket.sClose: not yet implemented")
 }
 
-func ExternalNetwork_Socket_prim_connectToSocket(task *gocurry.Task){
+func ExternalNetworkDot_SocketDot_primUs_connectToSocket(task *gocurry.Task){
     panic("Network.Socket.connectToSocket: not yet implemented")
 }
